69012876481f1ee87808f40eea37043cc5156445
marcoshidalgonunes <marcoshidalgonunes@msn.com>
2025-02-13 18:59:03 -0300

    Added close

--- api/src/main/java/com/postgrestoopensearch/api/config/OpenSearchClientConfig.java
+++ api/src/main/java/com/postgrestoopensearch/api/config/OpenSearchClientConfig.java
@@ -1,10 +1,15 @@
 package com.postgrestoopensearch.api.config;
 
 import org.opensearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+
 import org.apache.http.HttpHost;
 import org.opensearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PreDestroy;
 
 @Configuration
 public class OpenSearchClientConfig {
@@ -16,5 +21,10 @@ public class OpenSearchClientConfig {
                 new HttpHost("localhost", 9200, "http")
             )
         );
+    }
+
+    @PreDestroy
+    public void closeClient() throws IOException {
+        client().close();
     }
 }
