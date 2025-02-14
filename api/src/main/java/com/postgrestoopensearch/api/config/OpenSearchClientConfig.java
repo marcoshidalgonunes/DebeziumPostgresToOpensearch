06e54720f4a9dc546141b7f25496111058258391
marcoshidalgonunes <marcoshidalgonunes@msn.com>
2025-02-14 17:09:02 -0300

    Configuration adjusts

--- api/src/main/java/com/postgrestoopensearch/api/config/OpenSearchClientConfig.java
+++ api/src/main/java/com/postgrestoopensearch/api/config/OpenSearchClientConfig.java
@@ -6,19 +6,28 @@ import java.io.IOException;
 
 import org.apache.http.HttpHost;
 import org.opensearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import jakarta.annotation.PreDestroy;
 
 @Configuration
+@EnableElasticsearchRepositories(basePackages = "com.postgrestoopensearch.connector.repositories")
 public class OpenSearchClientConfig {
+
+    @Value("${opensearch.host}")
+    String host;
+
+    @Value("${opensearch.scheme}")
+    String scheme;
 
     @Bean
     RestHighLevelClient client() {
         return new RestHighLevelClient(
             RestClient.builder(
-                new HttpHost("localhost", 9200, "http")
+                HttpHost.create(scheme + "://" + host)
             )
         );
     }
